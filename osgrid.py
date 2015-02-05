fbe4d21fa5076ebd82d21f418f3bfc4c5fa9c7f0
Matthew Earl <matthew.earl@gmail.com>
2015-02-05 12:22:39 +0000

    More writing of the osgrid module

--- osgrid.py
+++ osgrid.py
@@ -2,44 +2,99 @@ import math
 import numpy
 
 
+def _long_lat_to_cartesian(long_lat, a, b):
+    """
+    Convert long/lat to cartesian coordinates.
+
+    This implements formulas B1 - B5 in Appendix B of `A guide to coordinate
+    systems in Great Britain`
+    
+    """
+    e2 = 1. - a**2 / b**2
+    nu = a / math.sqrt(1. - e2 * math.sin(lat) ** 2)
+    H = 0
+    return numpy.matrix([(nu + H) * math.cos(lat) * math.cos(long),
+                         (nu + H) * math.cos(lat) * math.sin(long),
+                         ((1. - e2) * nu + H) * math.sin(lat)]).T
+
+
+def _cartesian_to_long_lat(v, a, b):
+    """
+    Convert long/lat to cartesian coordinates.
+
+    This implements formulas B6 - B8 in Appendix B of `A guide to coordinate
+    systems in Great Britain`
+
+    """
+
+    x, y, z = numpy.array(v).flatten()
+    e2 = 1. - a**2 / b**2
+
+    p = math.sqrt(x**2 + y**2)
+    lat = math.atan2(z, p * (1. - e2))
+    lat_prev = None
+    while lat_prev is None or abs(lat - lat_prev) > 10**-16: 
+        lat_prev = lat
+        nu = a / math.sqrt(1. - e2 * math.sin(lat) ** 2)
+        lat = math.atan2(z + e2 * nu * math.sin(lat), p)
+
+    long = math.atan2(y, x)
+
+    return long, lat
+
+
 def _helmert_transform(long_lat,
                        from_a, from_b,
                        to_a, to_b,
                        cx, cy, cz, s, rx, ry, rz):
+    """
+    Convert long/lat between ellipsoids.
 
+    {from,to}_{a,b} are the major/minor semi axes of the ellipsoids being
+    converted.
+
+    The other parameters are described here:
+        http://en.wikipedia.org/wiki/Helmert_transformation
+
+    """
     long, lat = long_lat
 
-    # Compute x = the point in cartesian space.
-    x = numpy.matrix([[math.cos(lat) * math.cos(long),
-                       math.cos(lat) * math.sin(long),
-                       (from_b**2 / from_a**2) * math.sin(lat)]]).T
-    x *= from_a / math.sqrt(1. -
-            (1. - from_b**2 / from_a**2) * math.sin(lat)**2)
+    # Compute the point in cartesian space.
+    v = _long_lat_to_cartesian((long, lat), from_a, from_b)
+
+    # Convert rotation parameters from arc-seconds into radians.
+    rx, ry, rz = (math.pi * r / (180. * 3600.) for r in (rx, ry, rz))
 
     # Compute R, the rotation matrix, and the translation vector c.
-    R = numpy.matrix([[1.0,  -rx,  ry],
+    R = numpy.matrix([[1.0,  -rz,  ry],
                       [rz,   1.0, -rx],
                       [-ry,   rx, 1.0]])
     c = numpy.matrix([[cx, cy, cz]]).T
 
-    # Transform x.
-    x = c + (1. + s * 10**-6) * R * x
+    # Transform v.
+    v = c + (1. + s * 10**-6) * R * v
+    
+    long, lat = _cartesian_to_long_lat(v, to_a, to_b)
 
-    # Project back to a latitude / longitude.
-    p = numpy.linalg.norm(x[:2, 0])
-    lat = math.arctan2(x[2, 0], p * to_b**2 / to_a**2)
-    lat_prev = 2. * math.pi
-    while abs(lat - lat_prev) > 10**-16: 
-        lat, lat_prev = lat_prev, lat
-        to_nu = to_a / math.sqrt(1-to_e2*sin(lat_prev)**2)
-        lat = arctan2(x[2, 0] + to_e2 * to_nu * math.sin(lat_prev), p)
+    return long, lat
 
     
-def _grs80_to_airy1830(long_lat):
+def _gsr80_to_airy1830(long_lat):
     """
-
+    Convert a long/lat on the GSR80 ellipsoid to a long/lat on the Airy1830
+    ellipsoid.
 
     """
+    airy1830_a, airy1830_b = 6377563.396, 6356256.909
+    gsr80_a, gsr80_b = 6378137.000, 6356752.3141
+    cx, cy, cz = 446.448, 125.157, -542.06
+    s = 20.4894
+    rx, ry, rz = -0.1502, -0.247, -0.8421
+
+    return _helmert_transform(long_lat,
+                              gsr80_a, gsr80_b,
+                              airy1830_a, airy1830_b,
+                              cx, cy, cz, s, rx, ry, rz)
 
 
 def _airy1830_long_lat_to_os_grid(long_lat):
@@ -122,8 +177,8 @@ def _airy1830_long_lat_to_os_grid(long_lat):
 
 
 def _wgs84_long_lat_to_os_grid(long_lat):
-
     long, lat = (x * math.pi / 180. for x in long_lat)
+    k
 
 
 def get_image_from_sphere_mapping(sphere_mapping):
